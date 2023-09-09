4161657af9db692fd47294b26edaed36a476d9d6
Mikhail Syvachenko <misha.sivachenko@gmail.com>
2023-09-10 00:12:32 +0200

    feat: accidentally made initial commit for this project from work-account, reinitialized

---
+++ OnlineShop/Controllers/ProductManagementSystemController.cs
@@ -0,0 +1,79 @@
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Models;
+
+namespace OnlineShop.Controllers;
+
+public class ProductManagementSystemController : Controller
+{
+    // TODO: Add Db connection + services for business logics
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    public IActionResult Save(ProductViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            model.Id = Guid.NewGuid();
+            return RedirectToAction("Created", model);
+        }
+
+        return RedirectToAction("Create");
+    }
+    
+    public IActionResult Created(ProductViewModel model)
+    {
+        return View(model);
+    }
+
+    public IActionResult All()
+    {
+        var products = new List<ProductViewModel>
+        {
+            new()
+            {
+                Currency = "USD", Id = Guid.NewGuid(), ProductDescription = "Test product", ProductName = "Socks",
+                ProductPrice = 100
+            }
+        };
+
+        return View(products);
+    }
+    
+    public IActionResult Edit(Guid id)
+    {
+        var productViewModel = new ProductViewModel
+        {
+            Id = id,
+            ProductName = "Sample Product",
+            ProductDescription = "Sample Description",
+            ProductPrice = 100.00m,
+            Currency = "USD"
+        };
+
+        return View(productViewModel);
+    }
+    
+    [HttpPost]
+    public IActionResult Update(ProductViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+
+            return RedirectToAction("All");
+        }
+
+        return View("Edit", model);
+    }
+
+    public IActionResult Delete(Guid id)
+    {
+        return RedirectToAction("Create");
+    }
+}
---
+++ OnlineShop/Models/ProductViewModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Models;
+
+public class ProductViewModel
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Product Name is required.")]
+    public string ProductName { get; set; } = default!;
+    
+    [Required(ErrorMessage = "Product Description is required.")]
+    public string ProductDescription { get; set; } = default!;
+
+    // TODO: Handle image in normal way - blob storage / CDN
+    public IFormFile? ProductPhoto { get; set; }
+
+    [Required(ErrorMessage = "Product Price is required.")]
+    public decimal ProductPrice { get; set; }
+
+    [Required(ErrorMessage = "Currency is required.")]
+    public string Currency { get; set; } = default!;
+}
