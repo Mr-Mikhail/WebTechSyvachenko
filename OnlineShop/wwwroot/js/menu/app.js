ec71902608e3ca44c15f46bb01c917c46fe3ae96
Mikhail Syvachenko <misha.sivachenko@gmail.com>
2023-09-18 19:45:12 +0200

    feat: slight update to script

--- OnlineShop/wwwroot/js/menu/app.js
+++ OnlineShop/wwwroot/js/menu/app.js
@@ -1,5 +1,8 @@
+let selectedDishes = [];
+
 function addToOrder(dish) {
     // Add logic to handle adding the dish with the specified ID to the order
     let object = JSON.parse(dish);
+    selectedDishes.push(object);
     console.log("Added dish with ID: " + object.Id + " to order.");
 }
