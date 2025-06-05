93ac7df2397132a3db3b2bb17c66a6b7c987d033
Mikhail <58738460+Mr-Mikhail@users.noreply.github.com>
2025-06-05 13:57:40 +0000

    Fix missing return in DishController

--- OnlineShop/Controllers/DishController.cs
+++ OnlineShop/Controllers/DishController.cs
@@ -33,7 +33,7 @@ public class DishController : Controller
     {
         var dish = (await _dishRepository.GetAsync(x => x.Id == id, FilteringOptions.AsNoTrackingInstance, token)).FirstOrDefault();
         if (dish == null)
-            RedirectToAction("Index");
+            return RedirectToAction("Index");
 
         var viewModel = _mapper.Map<DishViewModel>(dish);
         
